5968a882216bbe36e3cc24fa9746232ddde1793b
Alumno <Alumno@AULA29-8>
2018-10-08 12:28:53 +0200

    08-10-2018

---
+++ Encriptado/src/es/cj/encriptado/Encriptado.java
@@ -0,0 +1,83 @@
+package es.cj.encriptado;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Encriptado {
+private static Scanner sc = new Scanner(System.in);
+	
+public static void main(String[] args) {
+	// Entrada por teclado de seleccion
+	System.out.println("Elija 'encriptar' o 'desencriptar'");
+	// String encrip = sc.next();
+	String encrip = "encriptar"; // provisional ( para no introducir encriptar )
+	
+	// Inicializacion de variables a usar
+	// ArrayList
+	
+	ArrayList<String> salida = new ArrayList<String>();
+	ArrayList<String> salida2 = new ArrayList<String>();
+	// Operadores
+	
+	char aValue;
+	int aAscii;
+	String aRray = "";
+	int num1;
+	int num2;
+	int num3;
+	
+	// Condicional para encriptar
+	if (encrip.equalsIgnoreCase("encriptar")) {
+		// Entrada por teclado de la clave a encriptar
+		System.out.println("Introduzca la clave a encriptar");
+		String clave = sc.next();
+		
+		// For para ecuacion de encriptacion para cada caracter
+		for (int i=0; i < clave.length(); i++ ) {
+			aValue = clave.charAt(i);
+			aAscii = (int)aValue;
+			
+			num1 = aAscii;
+			num3 = (int) (Math.random() * 100) + 2; // 2 a 101
+			num1 = num1 + num3;
+			
+			for (int j=0; j < aAscii; j++ ) {
+				if (num1 >= 1) {
+				num2 = num1 % 2;
+				
+				num1 = num1 / 2;
+				
+				if (num2 == 0)
+					aRray = aRray + 1 ;
+				else
+					aRray = aRray + 0 ;
+				}
+			}
+			StringBuilder builder=new StringBuilder(aRray);
+			String aRrayInvertida=builder.reverse().toString();
+			
+			aRrayInvertida = aRrayInvertida + num3;
+			aRrayInvertida = aRrayInvertida + ";";
+			salida.add(aRrayInvertida);
+			aRray = "";
+		}		
+		for (int i=0; i < salida.size(); i++ ) {
+			
+			System.out.print(salida.get(i));
+			
+		}
+		
+	
+		// Condicional para desencriptar
+	}else if (encrip.equalsIgnoreCase("desencriptar")) {
+		// Entrada por teclado de la clave a desencriptar
+				System.out.println("Introduzca la clave a desencriptar");
+				String clave = sc.next();
+		
+		
+	}
+	
+	
+	
+}
+}
