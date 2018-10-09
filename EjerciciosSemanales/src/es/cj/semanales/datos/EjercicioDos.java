60ed475f4374a47f233a799c47a9feff723be1d1
Alumno <Alumno@AULA29-8>
2018-10-09 14:41:40 +0200

    09-10-2018

---
+++ EjerciciosSemanales/src/es/cj/semanales/datos/EjercicioDos.java
@@ -0,0 +1,45 @@
+package es.cj.semanales.datos;
+
+import java.util.Scanner;
+
+public class EjercicioDos {
+
+	// (09/10/18) Un obrero necesita calcular su salario semanal, el cual se obtiene de la siguiente manera: 
+	// Si trabaja 40 horas o menos se le paga 16€ por hora 
+	// Si trabaja más de 40 horas se le paga 16€ por cada una de las primeras 40 horas y 20€ por cada hora extra.
+	
+	public static Scanner sc = new Scanner(System.in);
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Horas : ");
+		int horas = sc.nextInt();
+		
+		int num1;
+		int num2;
+		
+		int paga;
+		
+		if (horas > 0 && horas <= 40) {
+			
+			paga = horas * 16;
+			
+			System.out.println(paga);
+			
+		}else if (horas > 40) {
+			
+			num1 = 40 * 16;
+			
+			num2 = (horas - 40) * 20;
+			
+			paga = num1 + num2;
+			
+			System.out.println(paga);
+			
+		}
+	
+		
+		
+	}
+	
+}
---
+++ EjerciciosSemanales/src/es/cj/semanales/datos/EjercicioUno.java
@@ -0,0 +1,31 @@
+package es.cj.semanales.datos;
+
+import java.util.Scanner;
+
+public class EjercicioUno {
+
+	// (09/10/18) Se solicita leer un número entero positivo y determinar si es un número de 1,2,3 o 4 cifras.
+	
+	public static Scanner sc = new Scanner(System.in);
+		
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Numero : ");
+		int numero = sc.nextInt();
+		
+		if (numero > 0 && numero < 10) {
+			System.out.println("Numero de una cifra");
+		}else if(numero >= 10 && numero < 100) {
+				System.out.println("Numero de dos cifras");
+		}else if(numero >= 100 && numero < 1000) {
+				System.out.println("Numero de tres cifras");
+		}else if(numero >= 1000 && numero < 10000) {
+				System.out.println("Numero de cuatro cifras");
+		}else if(numero > 9999) {
+			System.out.println("Se va a mas de 4 cifras");
+		}else
+			System.out.println("Numero no es positivo");
+	}
+
+}
