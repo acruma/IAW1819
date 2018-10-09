400d61e9c048b0cfb71b4e57e647a24d3bdbffbb
Alumno <Alumno@AULA29-8>
2018-10-09 11:04:31 +0200

    09-10-2018

--- EjerciciosIF/src/es/cj/ejerciciosif/datos/EjerciciosIfSiete.java
+++ EjerciciosIF/src/es/cj/ejerciciosif/datos/EjerciciosIfSiete.java
@@ -4,67 +4,56 @@ import java.util.Scanner;
 
 public class EjerciciosIfSiete {
 
-
-
-	// Pedir al usuario dia, mes y año y comprobar que la fecha es correcta
-	// incluidos bisiestos
-		
 	public static Scanner sc = new Scanner(System.in);
+	
+	// Introduzca una nota de 0 a 10 y mostrarla de la forma: insuficiente, suficiente, bien..
 	
 	public static void main(String[] args) {
 		
-		System.out.println("Año");
-		int ano = sc.nextInt();
-		System.out.println("Mes");
-		int mes = sc.nextInt();
-		System.out.println("Dia");
-		int dia = sc.nextInt();
+		System.out.println("Dame un numero");
+		double num = sc.nextDouble();
 		
-		if ( ano >= 0 && (mes >= 1 && mes <= 12) && (dia >= 1 && dia <=31) ) {
-			
-			switch (mes) {
-			case 1:
-			case 3:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 12:
-				if(dia>31) {
-					System.out.println("Fecha incorrecta");
-				}else
-					System.out.println("Fecha correcta");
-				
-				break;
-			case 2:
-				if((ano%4 == 0 && ano%100 != 0) || (ano%400 == 0) ) {
-					if(dia>29) {
-						System.out.println("Fecha incorrecta");
-					}else {
-						System.out.println("Fecha correcta");
-					}
-				}else {
-					if(dia>28)
-						System.out.println("Fecha incorrecta");
-					else
-						System.out.println("Fecha correcta");
-				}
-				break;
-			case 4:
-			case 6:
-			case 9:
-			case 11:
-				if(dia>30) {
-					System.out.println("Fecha incorrecta");
-				}else
-					System.out.println("Fecha correcta");
-				
-				break;
-			default:
-				System.out.println("Fecha incorrecta");
-				break;
-			}
-		}else
-			System.out.println("Fecha incorrecta");
-	}
+		if (num >= 0 && num < 5)
+			System.out.println("Insuficiente");
+		else if (num >= 5 && num < 6)
+			System.out.println("Suficiente");
+		else if (num >= 6 && num < 7)
+			System.out.println("Bien");
+		else if (num >= 7 && num < 9)
+			System.out.println("Notable");
+		else if (num >= 9 && num <= 10)
+			System.out.println("Sobresaliente");
+		else 
+			System.out.println("Numero incorrecto.");
+	
+	
+	
+		switch ((int)num) {
+		case 0:
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+			System.out.println("Insuficiente");
+			break;
+		case 5:
+			System.out.println("Suficiente");
+			break;
+		case 6:
+			System.out.println("Bien");
+			break;
+		case 7:
+		case 8:
+			System.out.println("Notable");
+			break;
+		case 9:
+		case 10:
+			System.out.println("Sobresaliente");
+			break;
+		default:
+			System.out.println("Numero incorrecto.");
+			break;
+		}
+	
+	}	
 }
