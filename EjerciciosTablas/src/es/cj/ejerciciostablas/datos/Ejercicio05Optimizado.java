8d4f3e25c761003528cac1c11105a387522416bb
Alumno <Alumno@AULA29-8>
2018-10-26 14:26:56 +0200

    26-10-2018

--- EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio05Optimizado.java
+++ EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio05Optimizado.java
@@ -1,13 +1,13 @@
 package es.cj.ejerciciostablas.datos;
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio05Optimizado {
 
 	private static Scanner sc = new Scanner(System.in);
-	
-	private static int numeros[];
+	private static int[] numeros;
 	
 	// Crear una tabla de n números y desplazarla una posición hacia abajo, es decir, 
 	// el primero pasa a ser el segundo, 
@@ -15,37 +15,44 @@ public class Ejercicio05Optimizado {
 	
 	public static void main(String[] args) {
 		
-		int cantidad;
-		
+		int tamanyo;
+
 		do {
 			System.out.print("Cantidad de Nº : ");
-			cantidad = sc.nextInt();
-		}while( cantidad<=0 );
+			tamanyo = sc.nextInt();
+		} while (tamanyo <= 0);
 		
-		inicializar();
+		numeros = new int [tamanyo];
+		
+		iniciar();
 		
 		mostrar();
 		
 		desplazar();
 		
 		mostrar();
-	
+		
 	}
 
 	private static void desplazar() {
-		int aux = numeros(numeros.length - 1);
+		// TODO Auto-generated method stub
+		int aux = numeros[numeros.length - 1];
+		for (int i = numeros.length - 1; i > 0; i--) {
+			numeros[i] = numeros[i - 1];
+		}
+		numeros[0] = aux;
 	}
 
 	private static void mostrar() {
-		System.out.println(Arrays.toString(numeros));		
+		// TODO Auto-generated method stub
+		System.out.println(Arrays.toString(numeros));
 	}
 
-	private static void inicializar() {
-		
+	private static void iniciar() {
+		// TODO Auto-generated method stub
 		for (int i = 0; i < numeros.length; i++) {
-			numeros[i] = sc.nextInt();
+			numeros[i] = new Random().nextInt(50) + 1;
 		}
-		
 	}
 
 }
--- EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio09.java
+++ EjerciciosTablas/src/es/cj/ejerciciostablas/datos/Ejercicio09.java
@@ -10,24 +10,67 @@ public class Ejercicio09 {
 	// Ordena las tablas. Fusiona las 2 tablas en una tabla de 2*n elementos de 
 	// forma que esta tabla esté ordenada.
 	
+	// Las uno y entonces le hago sort ( no es lo que pide ) Sin comentarios :-D 
+	
 	private static Scanner sc = new Scanner(System.in);
+	private static int numeros [];
+	private static int numeros2 [];
+	private static int fusionada [];
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		inicializar();
+		
+		ordenar();
+		
+		fusion();
+		
+		ordenar();;
+		
+		mostrar();
+	
+	}
 
+	
+
+	private static void mostrar() {
+		System.out.println(Arrays.toString(fusionada));		
+	}
+
+	private static void fusion() {
+		
+		
+	}
+	
+	/*private static void fusion() {
+		int contador = 0;
+		for (int i = 0; i < numeros.length; i++) {
+				fusionada[i] = numeros[i];
+		}
+		for (int i = numeros.length; i < fusionada.length; i++) {
+			fusionada[i] = numeros2[contador];
+			contador++;
+		}}
+	*/
+	
+	
+
+	private static void inicializar() {
 		System.out.print("Cantidad de Nº : ");
 		int cantidad = sc.nextInt();
-		int numeros [] = new int [cantidad];
-		int numeros2 [] = new int [cantidad];
-		
+		numeros = new int [cantidad];
+		numeros2 = new int [cantidad];
+		fusionada = new int [(cantidad*2)];
 		for (int i = 0; i < numeros.length; i++) {
-				numeros[i] = new Random().nextInt(10);
-				numeros2[i] = new Random().nextInt(10);
-		}
-		
-		
-		
-		
+			numeros[i] = new Random().nextInt(10);
+			numeros2[i] = new Random().nextInt(10);
+		}	
+	}
+
+	private static void ordenar() {
+		Arrays.sort(numeros);
+		Arrays.sort(numeros2);
+		// Arrays.sort(fusionada);
 	}
 
 }
